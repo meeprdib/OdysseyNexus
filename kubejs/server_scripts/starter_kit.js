91c2ce30cded1ba3c481b78bd3938182f2d61719
Davey <meeprdib@outlook.com>
2024-06-07 19:53:38 -0400

    Replaced starter kit mod with kjs script

---
+++ kubejs/server_scripts/starter_kit.js
@@ -0,0 +1,7 @@
+PlayerEvents.loggedIn(event => {
+    if (!event.player.stages.has('new_join')) {
+        event.stages.add('new_join')
+        event.entity.setItemSlot(4, 'sophisticatedbackpacks:netherite_backpack')
+    }
+}
+)
