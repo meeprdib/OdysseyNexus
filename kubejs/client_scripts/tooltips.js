cabf653c93e704794451ea9fe801f7952e88943f
Davey <meeprdib@outlook.com>
2024-08-02 16:07:26 -0400

    Re-added Arcane Debris spawning

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -1,4 +1,5 @@
 ItemEvents.tooltip(event => {
-    event.add(['minecraft:enchanting_table'], 'Enchanting is disabled in this modpack! Find other ways to make yourself more powerful.');
-    event.add(['kubejs:ruined_controller'], 'A once powerful machine worn down by time and neglect, used as a centerpiece for storage networks. Only good for scrap parts, unless one wants to repair it...');
+    // event.add(['minecraft:enchanting_table'], 'Enchanting is disabled in this modpack! Find other ways to make yourself more powerful.');
+    // event.add(['kubejs:ruined_controller'], 'A once powerful machine worn down by time and neglect, used as a centerpiece for storage networks. Only good for scrap parts, unless one wants to repair it...');
+    event.add(['irons_spellbooks:arcane_debris'], 'Useless in the modpack as of now, but there might be a use for this later. If I decide to remove it, you have gotten yourself a collectors item!');
 })
